1e6605892b294a3c1500b7a55d2ae290bb23fc0c
Victor Willian <victorwillian1012@gmail.com>
2024-02-08 22:18:37 -0300

    feat: :zap: Initial setup to neovim

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,10 @@
+-- show update gitline
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+}
---
+++ lua/plugins/rose-pine.lua
@@ -0,0 +1,16 @@
+return {{
+  "rose-pine/neovim",
+  name="rose-pine",
+  extend_background_behind_borders = true,
+  styles = {
+    bold = true,
+    italic = true,
+    -- transparency = true,
+  },
+  -- opts = {
+  --   transparent = true,
+  -- },
+  config = function()
+    vim.cmd("colorscheme rose-pine")
+  end
+}}
