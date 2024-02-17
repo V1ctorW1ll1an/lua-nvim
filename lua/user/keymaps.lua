f25a34791ab73b3843bf2c076ec79a10454a7c4f
Victor Willian <victorwillian1012@gmail.com>
2024-02-17 10:33:08 -0300

    chore: :white_check_mark: Remove harpoon

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -3,8 +3,6 @@ local vnoremap = require("user.keymap-utils").vnoremap
 local inoremap = require("user.keymap-utils").inoremap
 local tnoremap = require("user.keymap-utils").tnoremap
 local xnoremap = require("user.keymap-utils").xnoremap
-local harpoon_ui = require("harpoon.ui")
-local harpoon_mark = require("harpoon.mark")
 local utils = require("user.utils")
 
 local M = {}
@@ -127,48 +125,6 @@ nnoremap("gx", ":sil !open <cWORD><cr>", { silent = true })
 
 -- TSC autocommand keybind to run TypeScripts tsc
 nnoremap("<leader>tc", ":TSC<cr>", { desc = "[T]ypeScript [C]ompile" })
-
--- Harpoon keybinds --
--- Open harpoon ui
-nnoremap("<leader>ho", function()
-  harpoon_ui.toggle_quick_menu()
-end)
-
--- Add current file to harpoon
-nnoremap("<leader>ha", function()
-  harpoon_mark.add_file()
-end)
-
--- Remove current file from harpoon
-nnoremap("<leader>hr", function()
-  harpoon_mark.rm_file()
-end)
-
--- Remove all files from harpoon
-nnoremap("<leader>hc", function()
-  harpoon_mark.clear_all()
-end)
-
--- Quickly jump to harpooned files
-nnoremap("<leader>1", function()
-  harpoon_ui.nav_file(1)
-end)
-
-nnoremap("<leader>2", function()
-  harpoon_ui.nav_file(2)
-end)
-
-nnoremap("<leader>3", function()
-  harpoon_ui.nav_file(3)
-end)
-
-nnoremap("<leader>4", function()
-  harpoon_ui.nav_file(4)
-end)
-
-nnoremap("<leader>5", function()
-  harpoon_ui.nav_file(5)
-end)
 
 -- Git keymaps --
 nnoremap("<leader>gb", ":Gitsigns toggle_current_line_blame<cr>")
